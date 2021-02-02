585f280f612c4d0f0e931c97f31fb246efdeaa66
taegwang_kim <taegwang_kim@tmax.co.kr>
2021-02-02 17:03:37 +0900

    LifeCycle in class-based_component

--- src/App.js
+++ src/App.js
@@ -21,17 +21,41 @@ function FuncComp(props){
       <input type="button" value="random" onClick={
         function(){
           numberState(Math.random())
-        }.bind(this)
+        }
       }></input>
     </div>
   );
 }
 
+var classStyle = 'color:red'
+
 class ClassComp extends React.Component{
   state = {
     number:this.props.initNumber
   }
+
+
+  /*
+  componentWillMount() -> rendering이 되기 전, component들이 마운트(rendering) 되기 전, 무엇을 해보자고 할 때 선언하여 사용.
+  render() -> rendering이 되면서 할 수 있는 일을 정의 가능.
+  componentDidMount() -> rendering이 된 후 무엇을 하고 싶다!
+  shouldComponentUpdate(nextProps, nextState) => render를 호출할 필요가 있냐 없냐를 우리가
+  여기에 코드를 짜는 것을 통해서 할 수 있다.
+  png파일 참고.
+  */s
+  componentWillMount(){
+    console.log("%cclass => componentWillMount",classStyle);
+  }
+  componentDidMount(){
+    console.log("%cclass => componentDidMount", classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("%cclass => ComponentUpdate", classStyle);
+    return true;
+  }
+
   render(){
+    console.log("%cclass => render", classStyle);
     return(
       <div className="container">
         <h2>class style component</h2>
